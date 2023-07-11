7b76c7be5ecbf0565e27452b42829f1defec988b
jrmarcco <jrmarcco@gmail.com>
2023-07-11 13:57:32 +0800

    feat: add interval update time

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
+const KanbanBoard = ({ children }) => (
+  <main className="kanban-board">{children}</main>
+);
+
+const KanbanColumn = ({ children, className, title }) => {
+  const combinedClassName = `kanban-column ${className}`;
+  return (
+    <section className={combinedClassName}>
+      <h2>{title}</h2>
+      <ul>{children}</ul>
+    </section>
+  );
+};
+
+const MINUTE = 60 * 1000
+const HOUR = 60 * MINUTE
+const DAY = 24 * HOUR
+const UPDATE_INTERVAL = MINUTE
 const KanbanCard = ({ title, status }) => {
+  const [displayTime, setDisplayTime] = useState(status);
+  useEffect(() => {
+
+    const updateDisplayTime = () => {
+      const passedTime = new Date() - new Date(status);
+      let relativeTime = 'Just'
+
+      if (MINUTE <= passedTime && passedTime < HOUR) {
+        relativeTime = `${Math.ceil(passedTime / MINUTE)} minutes ago`;
+      } else if (HOUR <= passedTime && passedTime < DAY) {
+        relativeTime = `${Math.ceil(passedTime / HOUR)} hours ago`;
+      } else if (passedTime >= DAY) {
+        relativeTime = `${Math.ceil(passedTime / DAY)} days ago`;
+      }
+
+      setDisplayTime(relativeTime);
+    };
+
+    const intervalId = setInterval(updateDisplayTime, UPDATE_INTERVAL);
+    updateDisplayTime();
+
+    return function () {
+      clearInterval(intervalId)
+    }
+
+  }, [status]);
+
   return (
     <li className="kanban-card">
       <div className="card-title">{title}</div>
-      <div className="card-status">{status}</div>
+      <div className="card-status" title={status}>{displayTime}</div>
     </li>
   );
 }
-
 const KanbanNewCard = ({ onSubmit }) => {
   const [title, setTitle] = useState('');
 
@@ -26,7 +70,7 @@ const KanbanNewCard = ({ onSubmit }) => {
 
   return (
     <li className="kanban-card">
-      <h3>添加新卡片</h3>
+      <h3>New Card</h3>
       <div className="kanban-card">
         <input type="text" value={title} onChange={handleChange} onKeyDown={handleKeyDown}/>
       </div>
@@ -49,12 +93,19 @@ function App() {
   const handleSubmit = (title) => {
 
     setTodoList(currentTodoList => [
-      { title, status: new Date().toDateString() },
+      { title, status: new Date().toLocaleString() },
       ...currentTodoList
     ]);
 
     setShowAdd(false);
   };
+
+  const todoTitle = (
+    <>
+      <span>Todo</span>
+      <button onClick={handleAdd} disabled={showAdd}>&#8853; 添加新卡片</button>
+    </>
+  );
 
   return (
     <div className="App">
@@ -62,27 +113,18 @@ function App() {
         <h1>React Learning</h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <main className="kanban-board">
-        <section className="kanban-column column-todo">
-          <h2>待处理<button onClick={handleAdd} disabled={showAdd}>&#8853; 添加新卡片</button></h2>
-          <ul>
-            { showAdd && <KanbanNewCard onSubmit={handleSubmit}/> }
-            { todoList.map(props => <KanbanCard {...props}/>) }
-          </ul>
-        </section>
-        <section className="kanban-column column-ongoing">
-          <h2>进行中</h2>
-          <ul>
-            { ongoingList.map(props => <KanbanCard {...props}/>) }
-          </ul>
-        </section>
-        <section className="kanban-column column-done">
-          <h2>已完成</h2>
-          <ul>
-            { doneList.map(props => <KanbanCard {...props}/>) }
-          </ul>
-        </section>
-      </main>
+      <KanbanBoard>
+        <KanbanColumn className="column-todo" title={todoTitle}>
+          { showAdd && <KanbanNewCard onSubmit={handleSubmit}/> }
+          { todoList.map(props => <KanbanCard key={props.title} {...props}/>) }
+        </KanbanColumn>
+        <KanbanColumn className="column-ongoing" title="Ongoing">
+          { ongoingList.map(props => <KanbanCard key={props.title} {...props}/>) }
+        </KanbanColumn>
+        <KanbanColumn className="column-done" title="Done">
+          { doneList.map(props => <KanbanCard key={props.title} {...props}/>) }
+        </KanbanColumn>
+      </KanbanBoard>
     </div>
   );
 }
